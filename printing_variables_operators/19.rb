11ef904d3002c2a89b359b0948a5b74e68215640
Cheysser <cheysser@gmail.com>
2024-09-09 23:22:13 +0800

    Ask user to input 2 numbers then multiply and print

---
+++ printing_variables_operators/19.rb
@@ -0,0 +1,7 @@
+
+puts "Give me a number."
+num_1 = gets.to_i
+puts "Give me another number."
+num_2 = gets.to_i
+answer = num_1 * num_2
+puts "When you multiply those 2 numbers, the answer is #{answer}."
