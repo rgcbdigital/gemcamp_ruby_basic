5fab8cbaaa1626bdfabb6f915ab004e79e6f0d0e
rgcbdigital <cheysser@gmail.com>
2024-09-10 21:39:12 +0800

    print lenght of 5 favorite movies

---
+++ array_and_hashes/activity_7.rb
@@ -0,0 +1,2 @@
+favorite_movies = ["Lord of the Rings - The Fellowship of the Ring", "The Lord of the Rings - The Two Towers", "The Lord of the Rings - Return of The King", "The Hobbit - An Unexpected Journey", "The Hobbit - The Desolation of Smaug"]
+print favorite_movies.length
