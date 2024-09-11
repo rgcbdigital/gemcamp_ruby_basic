308752b32a3c338d8669ba84ffaebd5c4990a945
rgcbdigital <cheysser@gmail.com>
2024-09-11 20:16:17 +0800

    check if 2 numbers given are equal

---
+++ conditional_statements/activity_3.rb
@@ -0,0 +1,7 @@
+puts "Input a number."
+number_1 = gets.chomp.to_i
+puts "Thank you! Can you give me another number?"
+number_2 = gets.chomp.to_i
+if number_1 == number_2
+  print "The numbers are equal."
+end
