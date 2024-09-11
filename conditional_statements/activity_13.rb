deb339a63d761965a2d90262501583110fc416b3
rgcbdigital <cheysser@gmail.com>
2024-09-11 20:56:07 +0800

    checks if two numbers are equal or not

---
+++ conditional_statements/activity_13.rb
@@ -0,0 +1,8 @@
+puts "Please give me two numbers:"
+number_1 = gets.chomp
+puts "Please give me the second number:"
+number_2 = gets.chomp
+if number_1 == number_2
+  print "The numbers are equal."
+else print "The numbers are not equal."
+end
