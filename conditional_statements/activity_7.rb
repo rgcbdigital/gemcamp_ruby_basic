96a5a8b32353a9a98271ee48229e19277f859ad7
rgcbdigital <cheysser@gmail.com>
2024-09-11 20:34:33 +0800

    checks if 2 words given are identical

---
+++ conditional_statements/activity_7.rb
@@ -0,0 +1,7 @@
+puts "Please provide 2 words. What's the first word?"
+word_1 = gets.chomp
+puts "Thank you! Now give me the second word."
+word_2 = gets.chomp
+if word_1 == word_2
+  print "The words are identical."
+end
