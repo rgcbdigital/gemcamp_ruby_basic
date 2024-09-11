acd21dc76d0018a1f1eb94b2a07232dbe9bfd83b
rgcbdigital <cheysser@gmail.com>
2024-09-11 11:56:16 +0800

    create hash 3 books print 2nd book

---
+++ array_and_hashes/activity_23.rb
@@ -0,0 +1,12 @@
+favorite_books = [
+  {
+    book: 1, title: "Lord of the Rings", author: "JRR Tolkien"
+  },
+  {
+    book: 2,title: "Harry Potter", author: "JK Rowling"
+  },
+  {
+    book: 3,title: "The Hobbit", author: "JRR Tolkien"
+  }
+]
+print favorite_books[1]
