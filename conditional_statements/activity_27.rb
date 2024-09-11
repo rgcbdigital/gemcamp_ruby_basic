6901fd14aadfa8531484fe9a8bfd0c24cacdc649
rgcbdigital <cheysser@gmail.com>
2024-09-11 22:59:56 +0800

    checks if string is empty or only space

--- conditional_statements/activity_27.rb
+++ conditional_statements/activity_27.rb
@@ -1,6 +1,6 @@
-puts "Can you enter a word?"
+puts "Please enter a string?"
 text = gets.chomp
-if text.empty? || text.include?(" ")
+if text.strip.empty?
   print "The string is empty or only contains spaces."
 else print "The string has valid content."
 end
