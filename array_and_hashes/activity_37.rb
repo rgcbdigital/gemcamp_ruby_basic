a7796cc5cd14af0528ba2e5b40dfacb8324dba7c
rgcbdigital <cheysser@gmail.com>
2024-09-11 17:29:35 +0800

    create array of hashes for students then print 10th name

---
+++ array_and_hashes/activity_37.rb
@@ -0,0 +1,14 @@
+student_info = [
+  { name: "John", age: 15, grade: 90 },
+  { name: "Jane", age: 20, grade: 75 },
+  { name: "Mary", age: 16, grade: 96 },
+  { name: "Sally", age: 17, grade: 97 },
+  { name: "Johnson", age: 18, grade: 89 },
+  { name: "Mark", age: 15, grade: 83 },
+  { name: "Jels", age: 18, grade: 85 },
+  { name: "Lao", age: 20, grade: 85 },
+  { name: "Fu", age: 19, grade: 90 },
+  { name: "Kang", age: 16, grade: 91 },
+]
+
+print student_info[9][:name]
