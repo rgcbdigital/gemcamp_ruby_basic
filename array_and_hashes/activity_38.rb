200a2d634c920bc3771cd4680a8741d899303f43
rgcbdigital <cheysser@gmail.com>
2024-09-11 18:21:12 +0800

    printed date of birth using elements in array

---
+++ array_and_hashes/activity_38.rb
@@ -0,0 +1,9 @@
+digits = [
+  [1, 2, 3],
+  [4, 5, 6],
+  [7, 8, 9],
+  ['-', 0]
+]
+print "(#{digits[0][0]}#{digits[2][2]}#{digits[2][2]}#{digits[0][0]}\
+-#{digits[0][0]}#{digits[0][1]}\
+-#{digits[0][2]}#{digits[0][0]})"
