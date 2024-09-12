e1b940946bcad452ab52cd8e310c357c7196d2ad
rgcbdigital <cheysser@gmail.com>
2024-09-12 16:59:27 +0800

    checks number to see day of the week

---
+++ conditional_statements/case_statement_keyword/Activity_1.rb
@@ -0,0 +1,21 @@
+puts  "From 1 to 7, give me a day of the week."
+
+day = gets.to_i
+
+
+case day
+when 1
+  puts "Monday"
+when 2
+  puts "Tuesday"
+when 3
+  puts "Wednesday"
+when 4
+  puts "Thursday"
+when 5
+  puts "Friday"
+when 6
+  puts "Saturday"
+when 7
+  puts "Sunday"
+end
